8e7aee768747c3dc8a594c273391e40ac3948f59
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-02-28 16:14:20 +0700

    Update main.c

--- src/main.c
+++ src/main.c
@@ -5,13 +5,19 @@
 #define PWM_CHANNELS 6
 #define SBUS_CHANNELS 16
 
+/* ================= HANDLES ================= */
+
 UART_HandleTypeDef huart1;
 TIM_HandleTypeDef htim2;
+TIM_HandleTypeDef htim3;
 TIM_HandleTypeDef htim4;
 
 /* ================= GLOBAL ================= */
 
 uint16_t pwm_value[PWM_CHANNELS] = {1500,1500,1500,1500,1500,1500};
+uint32_t rise_time[PWM_CHANNELS];
+uint8_t capture_state[PWM_CHANNELS];
+
 uint16_t sbus_channel[SBUS_CHANNELS];
 uint8_t sbus_frame[25];
 
@@ -21,11 +27,10 @@ uint16_t convert_to_sbus(uint16_t pwm)
 {
     if(pwm < 1000) pwm = 1000;
     if(pwm > 2000) pwm = 2000;
-
     return (uint16_t)((pwm - 1000) * 1639 / 1000 + 172);
 }
 
-void sbus_build_frame()
+void sbus_build_frame(void)
 {
     sbus_frame[0] = 0x0F;
 
@@ -40,7 +45,7 @@ void sbus_build_frame()
     sbus_frame[9]  = (sbus_channel[5] >> 9);
 
     for(int i=10;i<23;i++)
-        sbus_frame[i]=0;
+        sbus_frame[i] = 0;
 
     sbus_frame[23] = 0x00;
     sbus_frame[24] = 0x00;
@@ -64,10 +69,53 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
     }
 }
 
+/* ================= PWM CAPTURE ================= */
+
+void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
+{
+    uint32_t value;
+    uint8_t ch_index = 0;
+    uint32_t channel = 0;
+
+    if(htim->Instance == TIM2)
+    {
+        if(htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1){ ch_index=0; channel=TIM_CHANNEL_1;}
+        else if(htim->Channel == HAL_TIM_ACTIVE_CHANNEL_2){ ch_index=1; channel=TIM_CHANNEL_2;}
+        else if(htim->Channel == HAL_TIM_ACTIVE_CHANNEL_3){ ch_index=2; channel=TIM_CHANNEL_3;}
+        else if(htim->Channel == HAL_TIM_ACTIVE_CHANNEL_4){ ch_index=3; channel=TIM_CHANNEL_4;}
+    }
+    else if(htim->Instance == TIM3)
+    {
+        if(htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1){ ch_index=4; channel=TIM_CHANNEL_1;}
+        else if(htim->Channel == HAL_TIM_ACTIVE_CHANNEL_2){ ch_index=5; channel=TIM_CHANNEL_2;}
+    }
+
+    value = HAL_TIM_ReadCapturedValue(htim, channel);
+
+    if(capture_state[ch_index] == 0)
+    {
+        rise_time[ch_index] = value;
+        capture_state[ch_index] = 1;
+        __HAL_TIM_SET_CAPTUREPOLARITY(htim, channel, TIM_INPUTCHANNELPOLARITY_FALLING);
+    }
+    else
+    {
+        if(value >= rise_time[ch_index])
+            pwm_value[ch_index] = value - rise_time[ch_index];
+        else
+            pwm_value[ch_index] = (0xFFFF - rise_time[ch_index]) + value;
+
+        capture_state[ch_index] = 0;
+        __HAL_TIM_SET_CAPTUREPOLARITY(htim, channel, TIM_INPUTCHANNELPOLARITY_RISING);
+    }
+}
+
 /* ================= INIT ================= */
 
 void SystemClock_Config(void);
 static void MX_USART1_UART_Init(void);
+static void MX_TIM2_Init(void);
+static void MX_TIM3_Init(void);
 static void MX_TIM4_Init(void);
 
 int main(void)
@@ -76,7 +124,17 @@ int main(void)
     SystemClock_Config();
 
     MX_USART1_UART_Init();
+    MX_TIM2_Init();
+    MX_TIM3_Init();
     MX_TIM4_Init();
+
+    HAL_TIM_IC_Start_IT(&htim2, TIM_CHANNEL_1);
+    HAL_TIM_IC_Start_IT(&htim2, TIM_CHANNEL_2);
+    HAL_TIM_IC_Start_IT(&htim2, TIM_CHANNEL_3);
+    HAL_TIM_IC_Start_IT(&htim2, TIM_CHANNEL_4);
+
+    HAL_TIM_IC_Start_IT(&htim3, TIM_CHANNEL_1);
+    HAL_TIM_IC_Start_IT(&htim3, TIM_CHANNEL_2);
 
     HAL_TIM_Base_Start_IT(&htim4);
 
@@ -85,7 +143,7 @@ int main(void)
     }
 }
 
-/* ================= UART SBUS ================= */
+/* ================= UART ================= */
 
 static void MX_USART1_UART_Init(void)
 {
@@ -102,30 +160,7 @@ static void MX_USART1_UART_Init(void)
 
     HAL_UART_Init(&huart1);
 
-    /* TX Inversion */
-    SET_BIT(huart1.Instance->CR2, USART_CR2_TXINV);
-}
-
-/* ================= TIMER 7ms ================= */
-
-static void MX_TIM4_Init(void)
-{
-    __HAL_RCC_TIM4_CLK_ENABLE();
-
-    htim4.Instance = TIM4;
-    htim4.Init.Prescaler = 83;
-    htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
-    htim4.Init.Period = 7000-1;
-    htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-
-    HAL_TIM_Base_Init(&htim4);
-    HAL_NVIC_SetPriority(TIM4_IRQn, 0, 0);
-    HAL_NVIC_EnableIRQ(TIM4_IRQn);
-}
-
-void TIM4_IRQHandler(void)
-{
-    HAL_TIM_IRQHandler(&htim4);
+    // KHÔNG TXINV
 }
 
 /* ================= CLOCK ================= */
@@ -146,7 +181,10 @@ void SystemClock_Config(void)
 
     HAL_RCC_OscConfig(&RCC_OscInitStruct);
 
-    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_SYSCLK|RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_SYSCLK|
+                                  RCC_CLOCKTYPE_PCLK1|
+                                  RCC_CLOCKTYPE_PCLK2;
+
     RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
     RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
     RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
