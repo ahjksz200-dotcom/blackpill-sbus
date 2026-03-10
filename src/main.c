c441c23b7b6349ab723c80703987619d1f052880
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-03-10 15:17:07 +0700

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,58 +1,64 @@
-#define PPM_PIN PA0
+#include <Arduino.h>
+
 #define CHANNELS 8
+#define SYNC_PULSE 3000
 
 volatile uint16_t ppm[CHANNELS];
 volatile uint8_t channel = 0;
 
 volatile uint32_t lastTime = 0;
 
-void ppmISR() {
+void ppmISR()
+{
+    uint32_t now = micros();
+    uint32_t diff = now - lastTime;
+    lastTime = now;
 
-  uint32_t now = micros();
-  uint32_t diff = now - lastTime;
-  lastTime = now;
-
-  if (diff > 3000) { 
-    // Sync pulse
-    channel = 0;
-  }
-  else {
-    if (channel < CHANNELS) {
-      ppm[channel] = diff;
-      channel++;
+    if (diff > SYNC_PULSE)
+    {
+        channel = 0;
     }
-  }
+    else
+    {
+        if (channel < CHANNELS)
+        {
+            ppm[channel] = diff;
+            channel++;
+        }
+    }
 }
 
-void setup() {
+void setup()
+{
+    Serial.begin(115200);
 
-  Serial.begin(115200);
+    pinMode(PPM_PIN, INPUT_PULLUP);
 
-  pinMode(PPM_PIN, INPUT_PULLUP);
+    attachInterrupt(
+        digitalPinToInterrupt(PPM_PIN),
+        ppmISR,
+        RISING
+    );
 
-  attachInterrupt(
-    digitalPinToInterrupt(PPM_PIN),
-    ppmISR,
-    RISING
-  );
+    Serial.println("PPM reader start");
 }
 
-void loop() {
+void loop()
+{
+    static uint32_t t = 0;
 
-  static uint32_t t = 0;
+    if (millis() - t > 200)
+    {
+        t = millis();
 
-  if (millis() - t > 200) {
+        Serial.print("PPM: ");
 
-    t = millis();
+        for (int i = 0; i < CHANNELS; i++)
+        {
+            Serial.print(ppm[i]);
+            Serial.print(" ");
+        }
 
-    Serial.print("PPM: ");
-
-    for (int i = 0; i < CHANNELS; i++) {
-
-      Serial.print(ppm[i]);
-      Serial.print(" ");
+        Serial.println();
     }
-
-    Serial.println();
-  }
 }
